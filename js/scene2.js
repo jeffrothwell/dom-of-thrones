48f2ca21488ede97d25d03dfaf8559075cc2ef27
jeffrothwell <rothwelljeff@gmail.com>
2018-02-09 12:22:03 -0500

    set stage image for scene 2

---
+++ js/scene2.js
@@ -0,0 +1,35 @@
+function scene2() {
+
+  // Setup Stage
+  stage = document.querySelector('#stage');
+  castle = document.querySelector('#castle');
+  castleImgSrc = castle.getAttribute('src');
+  stageImg = stage.querySelector('#stage-img');
+  stageImg.setAttribute('src', castleImgSrc);
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(function() {
+
+  })
+
+  // Stage direction 2
+
+  frame(function() {
+
+  })
+
+}
